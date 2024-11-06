60c16f5aaf61583af09d8b464f6dc777f610418e
Gabrielle Partch <gpartch1253@gmail.com>
2024-11-06 14:43:56 -0700

    updated docker-compose.yaml for current project, added other necessary files to run container

---
+++ ProjectSourceCode/src/resources/js/script.js
@@ -0,0 +1,8 @@
+const plant_search_card_button = document.getElementById("plant-search-card");
+const plant_reccomendation_button = document.getElementById();
+const my_garden_card_button = document.getElementById()
+
+function plantSearchCard()
+{
+
+}
