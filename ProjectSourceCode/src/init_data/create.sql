13727a95c254c88133635dec460e87fa2fe54df8
Gabrielle Partch <gpartch1253@gmail.com>
2024-11-13 20:10:33 -0700

    added first/last name fields to users table

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -14,7 +14,8 @@ CREATE TABLE plants(
 --lightlevel is rated out of 3, 1 being low, 3 being high, same scale applies to locations
 CREATE TABLE userInfo(
     user_id SERIAL PRIMARY KEY NOT NULL,
-    name VARCHAR(100),
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
     username VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password CHAR(60) NOT NULL
