edc43b34bbba82232e2ca2bd580dc37095c82d72
Gabrielle Partch <gpartch1253@gmail.com>
2024-11-21 14:58:01 -0700

    added alert button js functionality draft

--- ProjectSourceCode/src/resources/js/script.js
+++ ProjectSourceCode/src/resources/js/script.js
@@ -1,3 +1,10 @@
-Handlebars.registerHelper('increment', function(value) {
-    return parseInt(value) + 1;
-});
+// Handlebars.registerHelper('increment', function(value) {
+//     return parseInt(value) + 1;
+// });
+
+document.getElementById("close-alert").addEventListener("click", hideAlert);
+
+function hideAlert()
+{
+    const alert = document.getElementById('alert').classList.add('hidden');
+}
