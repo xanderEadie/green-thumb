9446f6d528dc7dbc69ada1f80765f011f6ad6ca3
Gabrielle Partch <gpartch1253@gmail.com>
2024-12-03 21:02:23 -0700

    debugged message js functions + updated message partial to match

--- ProjectSourceCode/src/resources/js/script.js
+++ ProjectSourceCode/src/resources/js/script.js
@@ -1,10 +1,7 @@
-// Handlebars.registerHelper('increment', function(value) {
-//     return parseInt(value) + 1;
-// });
-
-document.getElementById("close-alert").addEventListener("click", hideAlert);
-
+document.getElementById('close-alert').addEventListener('click', hideAlert);
 function hideAlert()
 {
-    const alert = document.getElementById('alert').classList.add('hidden');
+    console.log("hideAlert function called");
+    const alert = document.getElementById('alert');
+    alert.style.display = "none";
 }
