4cc85b9cceb9eb6341dc56058df96381ed720f44
Gabrielle Partch <gpartch1253@gmail.com>
2024-12-08 22:30:44 -0700

    updated so location data values can be null

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,6 +1,4 @@
 CREATE TABLE plants(
-    --temporary to fill in for plants for userInfo
-    --Ideally the API will have it's own table to take from and connect to users
     plant_id SERIAL PRIMARY KEY NOT NULL,
     common_name VARCHAR(150) NOT NULL,
     scientific_name VARCHAR(150) NOT NULL,
@@ -13,7 +11,6 @@ CREATE TABLE plants(
     edible BOOL,--CHANGE TO BOOL and update INSERT.SQL
     img VARCHAR(300)
 );
---lightlevel is rated out of 3, 1 being low, 3 being high, same scale applies to locations
 CREATE TABLE userInfo(
     user_id SERIAL PRIMARY KEY NOT NULL,
     first_name VARCHAR(100),
@@ -25,18 +22,11 @@ CREATE TABLE userInfo(
 CREATE TABLE location(
     location_id SERIAL PRIMARY KEY NOT NULL,
     user_id INT REFERENCES userInfo(user_id),
-    minHardiness INT NOT NULL,
-    maxHardiness INT NOT NULL,
-    watering VARCHAR(25) NOT NULL,
-    sunlight VARCHAR(25) NOT NULL
+    minHardiness INT,
+    maxHardiness INT,
+    watering VARCHAR(25),
+    sunlight VARCHAR(25)
 );
--- CREATE TABLE user_to_location(
---     user_id INT NOT NULL,
---     location_id INT NOT NULL,
---     PRIMARY KEY (user_id, location_id),
---     FOREIGN KEY (user_id) REFERENCES userInfo(user_id),
---     FOREIGN KEY (location_id) REFERENCES location(location_id)
--- );
 CREATE TABLE user_to_plants(
     user_id INT NOT NULL,
     plant_id INT NOT NULL
